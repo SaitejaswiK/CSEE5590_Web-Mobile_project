fc01f50c109b15c846b68a92b97eaab78b0dc49e
Liam F <liamf1764@gmail.com>
2020-05-14 08:01:11 -0500

    Add example JSON
    Added example JSON

--- Project_increment_3/Source/Calorie_tracker/app/src/main/java/com/sawapps/baymaxhealthcare/DoctorActivity.java
+++ Project_increment_3/Source/Calorie_tracker/app/src/main/java/com/sawapps/baymaxhealthcare/DoctorActivity.java
@@ -1,6 +1,7 @@
 package com.sawapps.baymaxhealthcare;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
@@ -25,6 +26,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,6 +42,30 @@ public class DoctorActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private List<DoctorsList> doctorsLists;
+
+    public String loadJSON(Context context) {
+        String json = null;
+        try {
+            InputStream is = context.getAssets().open("example.json");
+
+            int size = is.available();
+
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+
+    }
 
 
     @Override
@@ -77,8 +104,11 @@ public class DoctorActivity extends AppCompatActivity {
                 progressDialog.dismiss();
 
                 try {
-                    JSONObject jsonObject = new JSONObject(response);
-                    JSONArray array = jsonObject.getJSONArray("items");
+                    // Google places API costs money to actually continue to use, so
+                    // I made an example JSON file to show how it should look. The example file
+                    // is a result of an actual Places API call
+                    JSONObject jsonObject = new JSONObject(loadJSON(getApplicationContext()));
+                    JSONArray array = jsonObject.getJSONArray("results");
 
                     for (int i = 0; i < array.length(); i++) {
                         JSONObject jo = array.getJSONObject(i);
